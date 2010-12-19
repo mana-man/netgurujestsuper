f8403dfaa1fa90be94ea0cd54be6d60c1dfdf470
Mateusz <mana_man@o2.pl>
2010-12-19 22:38:24 +0100

    czysty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netgurujestsuper::Application.config.secret_token = '9658fe24922afbd906e0ded1965b61ee04102699d5a379ccb0084f0d718278475bc405c1f0e065e662e37449d9c44efe91ced5dbb766c4df100630a28506c485'
